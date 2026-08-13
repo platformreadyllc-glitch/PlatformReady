d3f33abd025385f33718b2eaaba999f415dbcea4
Steven Potter <platformreadyllc@gmail.com>
2026-08-13 14:51:06 -0600

    backend: register FirmwareModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PlatformModule } from './platform/platform.module';
 import { LiftingCastModule } from './liftingcast/liftingcast.module';
+import { FirmwareModule } from './firmware/firmware.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PlatformModule,
     LiftingCastModule,
+    FirmwareModule,
   ],
 })
 export class AppModule {}
